1adbcc3a3ebf4eae0abab4a2e11fe1e53a92ac6c
Steven Rosa <s.rosa21@icloud.com>
2022-05-25 16:02:06 -0400

    Create handleChange method to properly update state for Form input

--- task-manager-fe/src/App.js
+++ task-manager-fe/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Task from './Components/Task';
+import TaskForm from './Components/TaskForm';
 
 function App() {
   return (
     <div className="App">
       <Task />
+      <TaskForm />
     </div>
   );
 }
--- task-manager-fe/src/Components/TaskForm.js
+++ task-manager-fe/src/Components/TaskForm.js
@@ -3,14 +3,21 @@ import React, {useState} from "react";
 const initialForm = {
     title: '',
     dueDate: '',
+    description: '',
     
 }
 const TaskForm = () => {
-    const [form, setForm] = useState()
+    const [form, setForm] = useState(initialForm)
+
+    const handleChange = event => {
+        setForm({...initialForm, [event.target.name]: event.target.value})
+    }
     return(
         <form>
             <label>Title</label>
-            <input type="text" value={title} onChange={this.handleChange} />
+            <input name='title' type="text" value={form.title} onChange={handleChange} />
         </form>
     )
 }
+
+export default TaskForm

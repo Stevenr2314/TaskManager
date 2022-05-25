76e712702a5184a82ba40266c228a2741815ee18
Steven Rosa <s.rosa21@icloud.com>
2022-05-25 16:11:23 -0400

    Create more Form input tags, Create handleSubmit method to POST form data to BE

--- task-manager-fe/src/Components/TaskForm.js
+++ task-manager-fe/src/Components/TaskForm.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, {useState} from "react";
 
 const initialForm = {
@@ -12,10 +13,26 @@ const TaskForm = () => {
     const handleChange = event => {
         setForm({...initialForm, [event.target.name]: event.target.value})
     }
+
+    const handleSubmit = event => {
+        axios.post('http://localhost:5001/tasks', form)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => console.log(err))
+    }
     return(
-        <form>
-            <label>Title</label>
+        <form onSubmit={handleSubmit}>
+            <label>Title: </label>
             <input name='title' type="text" value={form.title} onChange={handleChange} />
+            <br />
+            <label>Due Date: </label>
+            <input name='dueDate' type="text" value={form.dueDate} onChange={handleChange} />
+            <br />
+            <label>Description:</label>
+            <input name='description' type="text" value={form.description} onChange={handleChange} />
+            <br />
+            <button type='submit' value='Submit'/>
         </form>
     )
 }
